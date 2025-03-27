cad367fe2c882d0a022f798ff0f18657485c30ab
ravihansa <ravihansad@gmail.com>
2025-03-27 14:46:55 +0530

    Create user table pagination related changes

--- src/components/userTable.jsx
+++ src/components/userTable.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { useReactTable, getCoreRowModel, flexRender, } from "@tanstack/react-table";
+import React, { useState } from "react";
+import {
+    useReactTable,
+    getCoreRowModel,
+    getPaginationRowModel,
+    flexRender
+} from "@tanstack/react-table";
 import "@/styles/userTable.css";
 
 const UserTable = ({ data }) => {
@@ -11,15 +16,26 @@ const UserTable = ({ data }) => {
         { accessorKey: "lName", header: "Last Name" },
     ];
 
+    const [pagination, setPagination] = useState({
+        pageIndex: 0,
+        pageSize: 10, // Number of rows per page
+    });
+
     const table = useReactTable({
         data,
         columns,
+        state: {
+            pagination,
+        },
+        onPaginationChange: setPagination,
         getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
     });
 
     return (
         <div className="table-container">
             <table className="user-table" border="1">
+                <caption className="table-caption">Users Data</caption>
                 <thead>
                     {table.getHeaderGroups().map((headerGroup) => (
                         <tr key={headerGroup.id}>
@@ -43,6 +59,39 @@ const UserTable = ({ data }) => {
                     ))}
                 </tbody>
             </table>
+            <div className="pagination-container">
+                <div className="pagination-controls">
+                    <button
+                        onClick={() => table.previousPage()}
+                        disabled={!table.getCanPreviousPage()}
+                    >
+                        Previous
+                    </button>
+                    <span>
+                        Page{" "}
+                        {table.getState().pagination.pageIndex + 1} of{" "}
+                        {table.getPageCount()}
+                    </span>
+                    <button
+                        onClick={() => table.nextPage()}
+                        disabled={!table.getCanNextPage()}
+                    >
+                        Next
+                    </button>
+                </div>
+                <select
+                    value={table.getState().pagination.pageSize}
+                    onChange={(e) => {
+                        table.setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[5, 10, 20, 30, 40, 50].map((pageSize) => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </div>
     );
 };
